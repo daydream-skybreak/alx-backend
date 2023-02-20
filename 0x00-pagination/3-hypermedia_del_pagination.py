dd96f319363c588c500faa23deb923db4a7f7c49
Samuel <samueldesta323@gmail.com>
2023-02-21 00:47:42 +0300

    added a documentation

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,7 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """"""
+        """gives consistent dataset even after the data has been deleted"""
         data = self.indexed_dataset()
         assert index is not None and index >= 0 and index <= max(data.keys())
         page_data = []
