0dfa04fe3b88ed7ed24b24ee9f5959d9341afcea
Samuel <samueldesta323@gmail.com>
2023-02-20 15:28:16 +0300

    added file 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,5 +4,6 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple:
+    """function to return start and end index"""
     end = page_size * page
     return end - page_size, end
