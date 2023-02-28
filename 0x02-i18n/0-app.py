61760f4ba1dfd214166f1879f121efa68061179b
Samuel <samueldesta323@gmail.com>
2023-02-28 10:40:45 +0300

    task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def hello():
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
