fe6cd157e1e2e8c7b96cc50b0cf19fa53d27e9b4
Samuel <samueldesta323@gmail.com>
2023-03-28 01:07:32 +0300

    added task 1

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,25 @@
+import { createClient, print } from 'redis';
+const redis = require('redis');
+const client = createClient();
+
+client.on('error', err => {
+  console.log(`Redis client not connected to the server: ${err.toString()}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool (schoolName, value) {
+  client.SET(schoolName, value, print)
+}
+
+function displaySchoolValue(schoolName) {
+  client.GET(schoolName, (err, reply) => {
+    console.log(reply)
+  })
+}
+
+displaySchoolValue('Holberton')
+setNewSchool('HolbertonSanFrancisco', '100')
+displaySchoolValue('HolbertonSanFrancisco')
