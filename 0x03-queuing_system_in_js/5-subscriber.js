a9109b67636c44398fe873d4643e631ac9baa902
Samuel <samueldesta323@gmail.com>
2023-03-29 08:19:59 +0300

    added task5

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node dev
+import {createClient} from 'redis';
+
+const client = createClient();
+const EXIT = 'KILL_SERVER';
+client.on('error', (err)=> {
+    console.log('Redis client not connected to the server:', err.toString())
+})
+
+client.on('connect', ()=> {
+    console.log('Redis client connected to the server')
+})
+
+client.subscribe('holberton school channel');
+
+client.on('message', (err, msg) => {
+    console.log(msg)
+    if (msg === EXIT) {
+        client.unsubscribe();
+        client.quit();
+    }
+})
